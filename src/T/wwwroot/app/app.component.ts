aca167d60f7ad635e10d4828c313fd051bdf7a79
damianpietkasoft <damianpietkasoft@outlook.com>
2016-11-19 23:21:40 +0100

    init commit

---
+++ src/T/wwwroot/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+import { Http, JsonpModule } from '@angular/http';
+import 'rxjs/Rx';
+
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello Angular!!</h1>'
+})
+export class AppComponent {
+
+}
